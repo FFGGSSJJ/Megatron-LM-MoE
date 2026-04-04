82c4cc34a3024ffae55476c0549026d81ebf4fe9
Guanshujie Fu <fuguan@student.ethz.ch>
2026-04-05 00:58:21 +0200

    enable delay_wgrad_compute and gradient_accumulation_fusion; add custom all_to_all dispatcher implementation

--- megatron/core/transformer/moe/experts.py
+++ megatron/core/transformer/moe/experts.py
@@ -35,7 +35,8 @@ from megatron.core.transformer.moe.moe_utils import (
     get_align_size_for_quantization,
 )
 from megatron.core.transformer.moe.experts_util import (
-    grouped_swiglu_mlp
+    grouped_swiglu_mlp,
+    ExpertsWgradScheduler,
 )
 from megatron.core.transformer.spec_utils import build_module
 from megatron.core.transformer.transformer_config import TransformerConfig
@@ -70,6 +71,15 @@ logger = logging.getLogger(__name__)
 
 class GroupedLinearFc1Interface(Protocol):
     """Interface for linear_fc1 module in TEGroupedMLP."""
+
+        # register delay wgrad computation
+        # check distribured_data_parallel.py
+        self.wgrad_accumulation_and_reduce_hooks = []
+        self.expert_wgrad_scheduler = None
+        if config.moe_use_custom_function and config.delay_wgrad_compute:
+            self.expert_wgrad_scheduler = ExpertsWgradScheduler(config.delay_wgrad_compute)
+            self.weight1.skip_backward_post_hook = True
+            self.weight2.skip_backward_post_hook = True
 
     def forward(
         self, permuted_local_hidden_states: torch.Tensor, tokens_per_expert: list[int], /
